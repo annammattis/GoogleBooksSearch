d378cf547adf848d5515516d014164fa2c07b2dc
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-06 12:37:39 -0400

    updated app and header

---
+++ react-express/client/my-app/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+const Header = () => {
+    return(
+        <header>
+            <i class="fad fa-books"></i>
+            <h1>Books</h1>
+        </header>
+    )
+}
+
+export default Header;
