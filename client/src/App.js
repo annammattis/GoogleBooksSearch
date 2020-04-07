12509e05d7a2f4550a102cea763d0fb195fcb4ca
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-06 22:39:54 -0400

    updated src

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import Header from '/Header';
 import Books from './Books'
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
@@ -10,6 +15,16 @@ class App extends Component {
           <Header />
           <Books />
       </div>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 function ctorName(val) { 
     return val.constructor && typeof val.constructor === 'function' ? val.constructor.name : null; 
   } 
+
+ctorName();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
