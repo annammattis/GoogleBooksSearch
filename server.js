5343940817c3531f6d685ea8b8017065241712ff
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-07 10:43:46 -0400

    updating packages

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
